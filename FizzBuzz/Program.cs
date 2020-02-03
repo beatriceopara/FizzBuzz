cc1ce0ff7f43c214d3d7ac69cdc1c26f9f7c6798
Beatrice <beatrice.opara@softwire.com>
2020-02-03 15:49:55 +0000

    Print Bang if multiple of 7

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,11 +6,24 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+            for (int i = 1; i <= 105; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
+                
+                if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine("FizzBuzzBang");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine("FizzBang");
+                }
+                else if (i % 5 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine("BuzzBang");
                 }
                 else if (i % 3 == 0)
                 {
@@ -19,6 +32,10 @@ namespace FizzBuzz
                 else if (i % 5 == 0)
                 { 
                     Console.WriteLine("Buzz");   
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("Bang");
                 }
                 else
                 {
