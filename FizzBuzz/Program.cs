01130d98134391506e07cdd35778d82168326a8e
Beatrice <beatrice.opara@softwire.com>
2020-02-03 15:18:54 +0000

    Print Fizz whenever the number is a multiple of 3

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,7 +8,15 @@ namespace FizzBuzz
         {
             for (int i = 1; i < 10; i++)
             {
-                Console.WriteLine(i);
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                { 
+                    Console.WriteLine(i);   
+                }
+                
             }
         }
     }
