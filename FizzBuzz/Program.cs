f2d45e7f0fe0afcfddb07443f35233c28741bb44
Beatrice <beatrice.opara@softwire.com>
2020-02-03 15:07:23 +0000

    Printed numbers 1 to 10

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,10 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hiya!");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
